dca4a0d11469821b2268b7fa66dc005668e78d9e
Megan Leet <megan.leet@iinet.net.au>
2023-02-20 16:55:01 +1100

    Update readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # ausmash-python
 Python wrapper for Ausmash API
 
+Wraps the API for [Ausmash](https://ausmash.com.au/), with type hints and automatic rate limit respecting and other nice things. You will need to provide your own API key, see AusmashAPISettings or [Pydantic docs](https://docs.pydantic.dev/usage/settings/) for configuring it (ausmash_api_key is the environment variable you want)
+
+More to come, I'm bad at readmes
--- ausmash/settings.py
+++ ausmash/settings.py
@@ -9,7 +9,7 @@ class AusmashAPISettings(BaseSettings):
 	api_key: str | None = None
 	endpoint: str = 'https://api.ausmash.com.au'
 	cache_timeout: timedelta | None = timedelta(days=2)
-	#TODO: Different cache timeouts for different URL prefixes, /elo should be days=7 and /games and /regions should be a long time (though the latter may have new cities added)
+	#TODO: Different cache timeouts for different URL prefixes, /elo should be days=7 (probably) and /games and /regions should be a long time (though the latter may have new cities added)
 	sleep_on_rate_limit: bool = True #Set to false if you just want to raise an error instead, I guess
 	
 	class Config:
