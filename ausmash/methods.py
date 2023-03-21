2d7173bc14caa9ed04158f3ca185ae323b4191ac
Megan Leet <megan.leet@iinet.net.au>
2023-03-21 14:56:14 +1100

    Make some more params in get_active_players optional, add only_count_main_bracket

--- ausmash/methods.py
+++ ausmash/methods.py
@@ -53,6 +53,7 @@ def __flatten_pocket_match_array(player: Player, d: MutableMapping[str, Any], cl
 	return matches
 
 def is_current_pr(player: Player, game: Game | str) -> bool:
+	"""Returns true if the player is on a currently active PR for a game, attempting to exclude player showcases"""
 	if isinstance(game, str):
 		game = Game(game)
 	rankings = Ranking.featuring_player(player)
@@ -72,6 +73,7 @@ def get_matches_of_player_in_game(player: Player, game: Game | str) -> Sequence[
 	return __flatten_pocket_match_array(player, call_api(f'pocket/player/matches/{player.id}/{game.id}'), PocketMatchWithPOV)
 
 def was_player_pr_during_time(player: Player, game: Game | str, start_date: date | None=None, end_date: date | None=None) -> bool:
+	"""Returns true if a player was on a PR that was current for a certain timeframe, excluding player showcases"""
 	if isinstance(game, str):
 		game = Game(game)
 	rankings = Ranking.featuring_player(player, start_date, end_date)
@@ -79,34 +81,50 @@ def was_player_pr_during_time(player: Player, game: Game | str, start_date: date
 	return any(ranking for ranking in rankings if ranking.game == game and not ranking.is_probably_player_showcase)
 
 def is_pr_win(self: _BasePocketMatch | Match) -> bool:
+	"""Returns true if a Match represents a win against a player that was PR at the time
+	This might not work properly…"""
 	if not self.loser:
 		#Assume anyone not in the database (or untagged, perhaps deliberately due to being a side event) was not PR
 		return False
 	#TODO: Not entirely sure start_date and end_date are being used correctly here
 	return was_player_pr_during_time(self.loser, self.game, start_date=self.date, end_date=self.date)
 
-def get_active_players(game: Game | str, region: Region | str, season_start: date | None=None, season_end: date | None=None, minimum_events_to_count: int=1, series_to_exclude: Collection[str] | None=None, only_count_locals: bool=True) -> Collection[Player]:
-	players: dict[Player, tuple[set[ID], set[ID]]] = {} #Locals tournament IDs, interstate tournament IDs
-	for elo in Elo.for_game(game, region):
-		#Basically just to filter by last active to make things easier, instead of just getting all players in region
-		if season_start and elo.last_active < season_start:
-			continue
-		for result in Result.results_for_player(elo.player, season_start, season_end):
-			tournament = result.tournament
-			is_local = tournament.region.short_name == (region.short_name if isinstance(region, Region) else region)
+def get_active_players(game: Game | str | None, region: Region | str | None, season_start: date | None=None, season_end: date | None=None, minimum_events_to_count: int=1, series_to_exclude: Collection[str] | None=None, only_count_locals: bool=True, only_count_main_bracket: bool=True) -> Collection[Player]:
+	"""Returns a list of players that are considered active according to certain optional parameters
+	This would not return any player that is too new to have Elo calculated if game is specified, as it uses that to check tournament activity
+	@param game: Only count players who entered events for a particular game, or None for all Smash players
+	@param region: Region to get active players from, or None for anywhere
+	@param season_start: Count tournaments starting from this date
+	@param season_end: Count tournaments up until this date, so you can get active players for a PR season
+	@param minimum_events_to_count: Require players to have attended this many tournaments to be considered active
+	@param series_to_exclude: If specified, list of TournamentSeries names to not count, e.g. an arcadian series if that makes sense for whatever you are using this method for
+	@param only_count_locals: Don't count tournaments outside of region
+	@param only_count_main_bracket: Don't count a player as attending a tournament if they only entered redemption, a side bracket, or doubles
+	"""
 
-			if only_count_locals and not is_local:
-				continue
+	players: dict[Player, tuple[set[ID], set[ID]]] = {} #Locals tournament IDs, interstate tournament IDs
+	
+	players_to_check = (e.player for e in Elo.for_game(game, region) if not season_start or (e.last_active >= season_start)) if game else Player.all(region)
+	for player in players_to_check:
+		for result in Result.results_for_player(player, season_start, season_end):
+			tournament = result.tournament
+			if region:
+				is_local = tournament.region.short_name == (region.short_name if isinstance(region, Region) else region)
+
+				if only_count_locals and not is_local:
+					continue
+			else:
+				is_local = True
 
 			if series_to_exclude and tournament.series.name in series_to_exclude:
 				continue
 			
 			event = result.event
-			if event.game.short_name != (game.short_name if isinstance(game, Game) else game):
+			if game and (event.game.short_name != (game.short_name if isinstance(game, Game) else game)):
 				continue
-			if event.is_redemption_bracket or event.is_side_bracket or event.type != EventType.Singles:
-				continue #Well… we don't really need to? Should a player count as active if they only ever enter redemption or side brackets or doubles
+			if only_count_main_bracket and (event.is_redemption_bracket or event.is_side_bracket or event.type != EventType.Singles):
+				continue
 
-			players.setdefault(elo.player, (set(), set()))[0 if is_local else 1].add(tournament.id)
+			players.setdefault(player, (set(), set()))[0 if is_local else 1].add(tournament.id)
 
 	return {p for p, (locals, interstates) in players.items() if len(locals) + (0 if only_count_locals else len(interstates)) >= minimum_events_to_count}	
