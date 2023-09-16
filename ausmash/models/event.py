38a142f9a954e3a3dd4229b3e903862497289838
Megan Leet <megan.leet@iinet.net.au>
2023-09-17 04:44:12 +1000

    Fall back on entrant name to find start.gg entrant if needed

--- ausmash/models/event.py
+++ ausmash/models/event.py
@@ -117,16 +117,16 @@ class Event(DictWrapper):
 		return match['tournament']
 	
 	@cached_property
-	def seeds(self) -> Mapping['Player', int] | None:
+	def seeds(self) -> Mapping['Player', int | None] | None:
 		"""Requires start.gg API key, gets player > seed number for this event, attempting to match start.gg player ID to Player
 		Will only do anything for singles events because otherwise ehhh gets weird
 		Otherwise returns None"""
-		if not self.startgg_slug:
+		if not self.startgg_slug or not self.tournament_startgg_slug:
 			return None
 		entrants = get_event_entrants(self.tournament_startgg_slug, self.startgg_slug)
 		startgg_id_seeds: dict[int, int] = {entrant['participants'][0]['player']['id']: entrant['seeds'][0]['seedNum'] for entrant in entrants if len(entrant['participants']) == 1 and len(entrant['seeds']) == 1}
+		startgg_tag_seeds: dict[int, int] = {entrant['participants'][0]['gamerTag'].lower(): entrant['seeds'][0]['seedNum'] for entrant in entrants if len(entrant['participants']) == 1 and len(entrant['seeds']) == 1}
 		from .result import Result #Bugger it, naughty import outside of the top to avoid circular nonsense
-		return {result.player: startgg_id_seeds.get(result.player.start_gg_player_id) if result.player else None for result in Result.results_for_event(self)}
-			
+		return {result.player: startgg_id_seeds.get(result.player.start_gg_player_id, startgg_tag_seeds.get(result.player_name.lower())) if result.player else None for result in Result.results_for_event(self)}
 
 __doc__ = Event.__doc__ or __name__
