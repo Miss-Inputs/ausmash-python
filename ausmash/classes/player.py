e68a8b6f61592c7e962e0d8e9e14e92d806fdcf9
Megan Leet <megan.leet@iinet.net.au>
2024-04-03 15:12:21 +1100

    reverse_lookup_startgg_player_id can take an int too

--- ausmash/classes/player.py
+++ ausmash/classes/player.py
@@ -15,18 +15,22 @@ from .game import Game
 from .region import Region
 
 
-class Player(Resource):	
+class Player(Resource):
 	"""Individual player who plays or has ever played a Super Smash Bros. game at a competitive event.
 	Not all players at an event need to be added to the database, but doing so makes Elo more accurate, etc"""
+
 	base_url = 'players'
 
 	@classmethod
-	def all(cls, region: Region | str | None=None) -> Collection['Player']:
+	def all(cls, region: Region | str | None = None) -> Collection['Player']:
 		"""Gets all players, or all players currently in a region"""
 		if region:
 			if isinstance(region, str):
 				return cls.wrap_many(call_api_json(f'players/byregion/{region}'))
-			return {cls(p).updated_copy({'Region': region._data}) for p in call_api_json(f'players/byregion/{region.short_name}')} #pylint: disable=protected-access
+			return {
+				cls(p).updated_copy({'Region': region._data})
+				for p in call_api_json(f'players/byregion/{region.short_name}')
+			}  # pylint: disable=protected-access
 		return cls.wrap_many(call_api_json('players'))
 
 	@classmethod
@@ -35,16 +39,25 @@ class Player(Resource):
 		:raises NotFoundException: If the region does not have a player with that name"""
 		region_short = region.short_name if isinstance(region, Region) else region
 		return cls(call_api_json(f'players/find/{name}/{region_short}'))
-		
+
 	@classmethod
 	def search(cls, query: str) -> Collection['Player']:
 		return cls.wrap_many(call_api_json('players/search', {'q': query}))
-		
+
 	@classmethod
-	def reverse_lookup_start_gg_player_id(cls, player_id: str, player_name_hint: str | None=None, player_region_hint: Region | str | None=None, *, skip_searching_all: bool=False) -> 'Player | None':
+	def reverse_lookup_start_gg_player_id(
+		cls,
+		player_id: str | int,
+		player_name_hint: str | None = None,
+		player_region_hint: Region | str | None = None,
+		*,
+		skip_searching_all: bool = False,
+	) -> 'Player | None':
 		"""Given a player ID from start.gg's API, returns an Ausmash Player that is associated with that Start.gg player, or None if could not be found
 		Will search for a player name first or get all players in a region first if hint for either is given, but these are not restrictive filters, it will fall back to checking all players if it cannot find a match
 		:raises HTTPError: If some other HTTP error occurs other than not being found"""
+		if isinstance(player_id, int):
+			player_id = str(player_id)
 		if isinstance(player_region_hint, str):
 			player_region_hint = Region(player_region_hint)
 		if player_name_hint:
@@ -56,11 +69,17 @@ class Player(Resource):
 				except NotFoundError:
 					pass
 
-			name_result = next((p for p in cls.search(player_name_hint) if str(p.start_gg_player_id) == player_id), None)
+			name_result = next(
+				(p for p in cls.search(player_name_hint) if str(p.start_gg_player_id) == player_id),
+				None,
+			)
 			if name_result:
 				return name_result
 		if player_region_hint:
-			region_result = next((p for p in cls.all(player_region_hint) if str(p.start_gg_player_id) == player_id), None)
+			region_result = next(
+				(p for p in cls.all(player_region_hint) if str(p.start_gg_player_id) == player_id),
+				None,
+			)
 			if region_result:
 				return region_result
 		if skip_searching_all:
@@ -76,8 +95,8 @@ class Player(Resource):
 		region_dict = self.get('Region')
 		if region_dict:
 			return Region(region_dict)
-		
-		#We may have region ID or region short name, ideally use both for our partial proxy
+
+		# We may have region ID or region short name, ideally use both for our partial proxy
 		partial = {}
 		region_id = self.get('RegionID')
 		region_short = self.get('RegionShort')
@@ -135,7 +154,7 @@ class Player(Resource):
 	def result_count(self) -> int:
 		"""Number of results for singles events this player has"""
 		return cast(int, self['ResultCount'])
-	
+
 	@property
 	def video_count(self) -> int:
 		"""The number of videos on Ausmash featuring this player across their career and all Smash games, though only including singles matches"""
@@ -147,25 +166,46 @@ class Player(Resource):
 		player_id: int | None = self['SmashGGPlayerID']
 		return IntID(player_id) if player_id else None
 
-	def compare_common_winrates(self, game: Game, other: 'Player', start_date: date | None=None, end_date: date | None=None) -> tuple[Sequence['WinRate'], Sequence['WinRate']]:
+	def compare_common_winrates(
+		self,
+		game: Game,
+		other: 'Player',
+		start_date: date | None = None,
+		end_date: date | None = None,
+	) -> tuple[Sequence['WinRate'], Sequence['WinRate']]:
 		params = {}
 		if start_date:
 			params['startDate'] = start_date.isoformat()
 		if end_date:
 			params['endDate'] = end_date.isoformat()
 		response = call_api_json(f'compare/{game.id}/{self.id}/{other.id}/winrates', params)
-		return WinRate.wrap_many(response['Player1WinRates']), WinRate.wrap_many(response['Player2WinRates'])
+		return WinRate.wrap_many(response['Player1WinRates']), WinRate.wrap_many(
+			response['Player2WinRates']
+		)
 
-	def compare_common_results(self, game: Game, other: 'Player', start_date: date | None=None, end_date: date | None=None) -> Sequence[tuple[Event, int, int]]:
+	def compare_common_results(
+		self,
+		game: Game,
+		other: 'Player',
+		start_date: date | None = None,
+		end_date: date | None = None,
+	) -> Sequence[tuple[Event, int, int]]:
 		"""(Event, player 1 result, player 2 result) for events that both of these players competed in"""
 		params = {}
 		if start_date:
 			params['startDate'] = start_date.isoformat()
 		if end_date:
 			params['endDate'] = end_date.isoformat()
-		return tuple((Event(result['Event']), result['Player1Result'], result['Player1Result']) for result in call_api_json(f'compare/{game.id}/{self.id}/{other.id}/results', params)['Results'])
-	
-	def get_win_rates(self, game: Game | str, start_date: date | None=None, end_date: date | None=None) -> Sequence['WinRate']:
+		return tuple(
+			(Event(result['Event']), result['Player1Result'], result['Player1Result'])
+			for result in call_api_json(f'compare/{game.id}/{self.id}/{other.id}/results', params)[
+				'Results'
+			]
+		)
+
+	def get_win_rates(
+		self, game: Game | str, start_date: date | None = None, end_date: date | None = None
+	) -> Sequence['WinRate']:
 		"""Win rates against every opponent this player has ever played a singles match against for a certain game, from highest Elo to lowest"""
 		params = {}
 		if start_date:
@@ -174,23 +214,30 @@ class Player(Resource):
 			params['endDate'] = end_date.isoformat()
 		if isinstance(game, str):
 			game = Game(game)
-		#I guess /pocket/player/winrates/{player ID}/{game ID} ['Items'] does the same thing but without date params?
+		# I guess /pocket/player/winrates/{player ID}/{game ID} ['Items'] does the same thing but without date params?
 		return WinRate.wrap_many(call_api_json(f'players/{self.id}/winrates/{game.id}', params))
 
-	def get_win_rate_dict(self, game: Game | str, start_date: date | None=None, end_date: date | None=None) -> Mapping['Player', 'WinRate']:
+	def get_win_rate_dict(
+		self, game: Game | str, start_date: date | None = None, end_date: date | None = None
+	) -> Mapping['Player', 'WinRate']:
 		"""get_win_rates as a mapping of {opponent: win rate}"""
-		return {win_rate.opponent: win_rate for win_rate in self.get_win_rates(game, start_date, end_date)}
-	
+		return {
+			win_rate.opponent: win_rate
+			for win_rate in self.get_win_rates(game, start_date, end_date)
+		}
+
 	@property
 	def pronouns(self) -> str | None:
 		"""Requires start.gg API key, gets the pronoun field from this player's start.gg profile, or None if they did not enter any or do not have a start.gg user profile"""
 		if not self.start_gg_player_id:
 			return None
 		pronouns = startgg_api.get_player_pronouns(self.start_gg_player_id)
-		return pronouns.capitalize() if pronouns else None #Ensure capitalisation consistency
+		return pronouns.capitalize() if pronouns else None  # Ensure capitalisation consistency
+
 
 class WinRate(DictWrapper):
 	"""Wins against a certain opponent, this is specific to a Player and Game"""
+
 	@property
 	def opponent(self) -> Player:
 		"""Opponent that this win rate pertains to"""
@@ -205,12 +252,12 @@ class WinRate(DictWrapper):
 	def wins(self) -> int:
 		"""Number of matches where this player wins againt opponent"""
 		return cast(int, self['Wins'])
-	
+
 	@property
 	def losses(self) -> int:
 		"""Number of matches where this player loses againt opponent"""
 		return cast(int, self['Losses'])
-	
+
 	@property
 	def total(self) -> int:
 		"""Equivalent to wins + losses, though the field is already there so we might as well use it and save uhhh adding two other dict items"""
@@ -221,5 +268,4 @@ class WinRate(DictWrapper):
 		"""This win rate as wins/total"""
 		return Fraction(self.wins, self.total)
 
-	#Percent is rounded to integer from 0-100, not really needed
-	
+	# Percent is rounded to integer from 0-100, not really needed
